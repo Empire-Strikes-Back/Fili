77caec1893a2fe3480b93f384fa2ad21e20edc5f
dgraph-explorer <dgraph-explorer@program>
2021-08-03 20:51:24 +0300

    i revert "for creating uberjar i use genie library"
    This reverts commit fe49452537926c62058fe48c953c27585b1ca118.

--- program.sh
+++ program.sh
@@ -17,14 +17,4 @@ main(){
     -M -m dgraph-explorer.main
 }
 
-uberjar(){
-  clj \
-    -X:uberjar genie.core/process \
-    :uberjar-name out/dgraph-explorer.standalone.jar \
-    :main-ns dgraph-explorer.main
-  mkdir -p out/jpackage-input
-  mv out/dgraph-explorer.standalone.jar out/jpackage-input/
-}
-
-
 "$@"
