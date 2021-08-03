bec88458c48890bd6afd283044b440eb0c3f7e49
dgraph-explorer <dgraph-explorer@program>
2021-08-03 20:51:17 +0300

    i revert "to program.sh i add j-package"
    This reverts commit 4fc6ad7a3e7f790b0ff8bccc7b346accfd2dc77b.

--- program.sh
+++ program.sh
@@ -26,31 +26,5 @@ uberjar(){
   mv out/dgraph-explorer.standalone.jar out/jpackage-input/
 }
 
-j-package(){
-  OS=${1:?"Need OS type (windows/linux/mac)"}
-
-  echo "Starting compilation..."
-
-  if [ "$OS" == "windows" ]; then
-    J_ARG="--win-menu --win-dir-chooser --win-shortcut"
-          
-  elif [ "$OS" == "linux" ]; then
-      J_ARG="--linux-shortcut"
-  else
-      J_ARG=""
-  fi
-
-  jpackage \
-    --input out/jpackage-input \
-    --dest out \
-    --main-jar dgraph-explorer.standalone.jar \
-    --name "dgraph-explorer" \
-    --main-class clojure.main \
-    --arguments -m \
-    --arguments dgraph-explorer.main \
-    --app-version "1" \
-    $J_ARG
-}
-
 
 "$@"
