6e4227cce9014e5fc178d9fd1a3ce41a21f53db3
Fili <Fili@tablewalker>
2022-07-03 23:23:34 +0300

    i am a program of 5 foods - fruit B12 salt oats dried-fruit :Lee-Mack Steve, you used to be rancid? :Steve-Davis certain areas still a bit

--- program.sh
+++ program.sh
@@ -1,7 +1,42 @@
 #!/bin/bash
 
 repl(){
-  clj -M:repl
+  clj \
+    -J-Dclojure.core.async.pool-size=8 \
+    -X:Ripley Ripley.core/process \
+    :main-ns Fili.main
+}
+
+
+main(){
+  clojure \
+    -J-Dclojure.core.async.pool-size=8 \
+    -M -m Fili.main
+}
+
+tag(){
+  COMMIT_HASH=$(git rev-parse --short HEAD)
+  COMMIT_COUNT=$(git rev-list --count HEAD)
+  TAG="$COMMIT_COUNT-$COMMIT_HASH"
+  git tag $TAG $COMMIT_HASH
+  echo $COMMIT_HASH
+  echo $TAG
+}
+
+jar(){
+
+  rm -rf out/*.jar out/classes
+  COMMIT_HASH=$(git rev-parse --short HEAD)
+  COMMIT_COUNT=$(git rev-list --count HEAD)
+  clojure \
+    -X:Genie Genie.core/process \
+    :main-ns Fili.main \
+    :filename "\"out/Fili-$COMMIT_COUNT-$COMMIT_HASH.jar\"" \
+    :paths '["src"]'
+}
+
+release(){
+  jar
 }
 
 "$@"
